d30370762ed0fb0194b1cc6fab20b555c522ff4a
eddva123 <support@eddva.in>
2026-07-06 12:08:21 +0530

    bhagyasree changes

--- metro.config.js
+++ metro.config.js
@@ -1,18 +1,10 @@
-<<<<<<< HEAD
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
-=======
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
->>>>>>> 19c8277 (Initial commit)
 
 /**
  * Metro configuration
  * https://reactnative.dev/docs/metro
  *
-<<<<<<< HEAD
  * @type {import('@react-native/metro-config').MetroConfig}
-=======
- * @type {import('metro-config').MetroConfig}
->>>>>>> 19c8277 (Initial commit)
  */
 const config = {};
 
